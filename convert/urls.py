35fc9d76bfb20e963d366e89f74540fd10b81503
Mirafzal <mirafzal@MacBook-Pro-Mirafzal.local>
2024-02-25 17:23:13 +0500

    done

--- convert/urls.py
+++ convert/urls.py
@@ -6,7 +6,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path("api/text/", views.ConvertAPIView.as_view(), name="user-list"),
     path("api/file/", views.ConvertFileAPIView.as_view(), name="text-convert"),
-    path("", views.somsa, name="index")
+    path("", views.main_page, name="index")
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
